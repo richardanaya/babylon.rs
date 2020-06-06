38820c208b5d35dd8afbf566e3b14caf517f2c9a
Richard <Richard>
2020-06-06 16:57:21 -0700

    basics

--- examples/helloworld/src/lib.rs
+++ examples/helloworld/src/lib.rs
@@ -1,6 +1,30 @@
 use babylon::*;
 
+#[macro_use]
+extern crate lazy_static;
+
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref GAME: Mutex<Game> = Mutex::new(Game::new());
+}
+
+struct Game {
+    scene: Scene,
+}
+
+impl Game {
+    fn new() -> Game {
+        Game {
+            scene: Scene::create_from_basic_engine("#renderCanvas"),
+        }
+    }
+
+    fn init(&mut self) {}
+}
+
 #[no_mangle]
-pub fn main() -> () {
-    js!(console.log).invoke_1("Hello World");
+pub fn main() {
+    let mut game = GAME.lock().unwrap();
+    game.init();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,32 @@
 pub use js_ffi::*;
+
+pub struct Scene {
+    scene_ref: JSValue
+}
+
+impl Scene {
+    pub fn create_from_basic_engine(selector:&str) -> Scene {
+        let func = register_function(r#"
+            function(selector){
+                var canvas = document.querySelector(selector);
+                var engine = new BABYLON.Engine(canvas, true); 
+                var createScene = function () {
+                    var scene = new BABYLON.Scene(engine);
+                    return scene;
+                };
+                var scene = createScene();
+                engine.runRenderLoop(function () {
+                        scene.render();
+                });
+                window.addEventListener("resize", function () {
+                        engine.resize();
+                });
+                return scene;
+            }
+        "#);
+        let scene_ref = func.invoke_1(selector);
+        Scene {
+            scene_ref
+        }
+    }
+}
