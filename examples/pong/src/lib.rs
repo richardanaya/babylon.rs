e347a356022b00d2ba5fc1a45c967968dd82c20f
Richard <Richard>
2020-06-07 21:34:13 -0700

    pong

--- examples/pong/src/lib.rs
+++ examples/pong/src/lib.rs
@@ -13,9 +13,10 @@ struct Game {
     _light_1: HemisphericLight,
     _light_2: PointLight,
     ball: Sphere,
-    _paddle_1: Cube,
+    paddle_1: Cube,
     paddle_2: Cube,
-    dir: f64,
+    paddle_dir: f64,
+    ball_dir: Vector,
 }
 
 impl Game {
@@ -29,9 +30,9 @@ impl Game {
         let mut paddle_mat = StandardMaterial::new(&scene);
         paddle_mat.set_diffuse_color(Color::new(0.5, 0.5, 0.5));
         paddle_mat.set_emmisive_color(Color::new(0.5, 0.5, 0.5));
-        let mut _paddle_1 = Cube::new(&scene, 0.5, 0.05, 0.05);
-        _paddle_1.set_position(Vector::new(0.0, 0.5, 0.0));
-        _paddle_1.set_material(&paddle_mat);
+        let mut paddle_1 = Cube::new(&scene, 0.5, 0.05, 0.05);
+        paddle_1.set_position(Vector::new(0.0, 0.5, 0.0));
+        paddle_1.set_material(&paddle_mat);
         let mut paddle_2 = Cube::new(&scene, 0.5, 0.05, 0.05);
         paddle_2.set_position(Vector::new(0.0, -0.5, 0.0));
         paddle_2.set_material(&paddle_mat);
@@ -41,39 +42,63 @@ impl Game {
             _light_1,
             _light_2,
             ball,
-            _paddle_1,
+            paddle_1,
             paddle_2,
-            dir: 0.0,
+            paddle_dir: 0.0,
+            ball_dir: Vector::new(babylon::js::random() - 0.5, -1.0, 0.0),
         }
     }
 
     fn run(&mut self, delta_time: f64) {
-        let p = self.ball.get_position();
-        let x = p.x;
+        // get positions
+        let p = self.paddle_1.get_position();
+        let p2 = self.paddle_2.get_position();
+        let bp = self.ball.get_position();
+
+        // move ball
+        let b_x = self.ball_dir.x * delta_time + bp.x;
+        let b_y = self.ball_dir.y * delta_time + bp.y;
+        if b_x > 0.5 || b_x < -0.5 {
+            self.ball_dir.x = -self.ball_dir.x;
+        }
+
+        if b_y > 0.75 || b_y < -0.75 {
+            self.ball.set_position(Vector::new(0.0, 0.0, 0.0));
+            self.ball_dir.x = babylon::js::random() - 0.5;
+            self.ball_dir.y = -self.ball_dir.y;
+        } else if b_y > 0.45 || (b_y < -0.45 && b_x <= p2.x + 0.25 && b_x >= p2.x - 0.25) {
+            self.ball_dir.y = -self.ball_dir.y;
+            self.ball.set_position(Vector::new(b_x, b_y, 0.0));
+        } else {
+            self.ball.set_position(Vector::new(b_x, b_y, 0.0));
+        }
+
+        // move opponent paddle to match ball
+        let x = b_x;
         let y = p.y;
         let z = p.z;
-        self.ball
-            .set_position(Vector::new(x, y + -1.0 * delta_time, z));
-        if self.dir != 0.0 {
-            let p = self.paddle_2.get_position();
-            let x = p.x;
-            let y = p.y;
-            let z = p.z;
+        self.paddle_1.set_position(Vector::new(x, y, z));
+
+        // move paddle if it has velocity
+        if self.paddle_dir != 0.0 {
+            let x = p2.x;
+            let y = p2.y;
+            let z = p2.z;
             self.paddle_2
-                .set_position(Vector::new(x + delta_time * self.dir, y, z))
+                .set_position(Vector::new(x + delta_time * self.paddle_dir, y, z));
         }
     }
 
     fn key_up(&mut self, _key_code: f64) {
-        self.dir = 0.0;
+        self.paddle_dir = 0.0;
     }
 
     fn key_down(&mut self, key_code: f64) {
         babylon::js::log(&format!("{}", key_code));
         if key_code == 37.0 {
-            self.dir = 1.0;
+            self.paddle_dir = 1.0;
         } else if key_code == 39.0 {
-            self.dir = -1.0;
+            self.paddle_dir = -1.0;
         }
     }
 }
